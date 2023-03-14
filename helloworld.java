f7dca5f010fa97afc52711b72cb099e7c2407740
Ashutosh Aswani <202051215@iiitvadodara.ac.in>
2023-03-14 22:53:24 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World Code from Ashutosh.");
+    System.out.println("Hello World Code 1 from Ashutosh.");
     }
 }
